e7376ce303d7c88eda5550426fce0f0d0e0ed57c
Jeka-Endorphin <jekamu@gmail.com>
2016-07-04 17:47:49 +0300

    added js_1-2 v.1.1

--- JavaScript/hw_1-2/js/script2.js
+++ JavaScript/hw_1-2/js/script2.js
@@ -10,22 +10,7 @@ for (var i = 0; i < myArr.length; i++) {
 	}
 };
 
-/*for (var i = 0; i < myArr.length; i++) {
-	myArr[i] = prompt('Введите ' + (i + 1) + '-е имя для внесения в список');
-	if (myArr[i] == null) {
-		alert('! Ввод отменён !');
-	} else {
-		while (myArr[i] == (undefined || '' || ' ')) {
-			myArr[i] = prompt('Введите же хоть какое-то значение :)', ' ');
-		}
-	}
-};*/
-
-
-console.log('Массив = ' + myArr);
-
 var userName = prompt('Укажите имя пользователя');
-console.log('userName: ' + userName);
 
 find(myArr, userName);
 

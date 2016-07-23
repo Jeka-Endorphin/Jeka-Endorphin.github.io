c57c34c5dedd706e532a6f40b9ae3bce28b7b02c
Jeka-Endorphin <jekamu@gmail.com>
2016-07-23 17:36:20 +0300

    added hw_7-8 task 2

--- JavaScript/hw_7-8/js/script.js
+++ JavaScript/hw_7-8/js/script.js
@@ -3,6 +3,7 @@
 $(function() {
   
   var $tabs = $('.area > div');
+  var $paragraphAll = $(".task1__wrapper p");
 
   $tabs.on('click', function(e) {
     $tabs.removeClass('active');
@@ -10,8 +11,23 @@ $(function() {
     var $tabClass = e.target.className;
     $tab.addClass('active');
     var $paragraph = $('p.'+ $tabClass); 
-    $(".wrapper p").hide();
-    $paragraph.show();
+    $paragraphAll.hide(500);
+    $paragraph.show('fast');
   })
 
+  var $inp = $('form input');
+  var $help = $('.help');
+  var $button = $('.task2 button');
+
+  $inp.hover(function (e) {
+    var $target = $(e.target);
+    $target.next().show('fast');
+  }, function (e) {
+    var $target = $(e.target);
+    $target.next().hide(1000);
+  })
+
+  $button.on('click', function (e) {
+    $help.show('fast');
+  })
 });

c50d35f22722095188042018c40f0476fd4d0e82
Jeka-Endorphin <jekamu@gmail.com>
2016-10-07 16:44:09 +0300

    added work

---
+++ task EP/dist/js/main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+$(function () {
+
+  // show account
+  $('.auth').one('click', function () {
+    $(this).animate({
+      opacity: 0 }, 500, function () {
+      $(this).hide();
+    });
+    $('.account').removeClass('account--hidden');
+  });
+
+  // account expanded
+  $('.account__user-name, .account__arrow').on('click', function () {
+    $(this).parent().toggleClass('account__inner-wrapper--expanded');
+  });
+
+  // address edit
+  $('.address-edit').on('click', function () {
+    $('.address-form').toggle('true');
+  });
+});
